e4e2f2af52d8b4c93017360e5c12e63dfd3bcc05
burjepaul <burjepaul97@gmail.com>
2022-07-22 11:33:56 +0300

    First commit

---
+++ src/components/Expenses/Expenses.component.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ExpenseItems from './ExpenseItems'
+import Card from '../UI/Card.component'
+import './Expenses.styles.css'
+
+export default function Expenses(props) {
+  return (
+    <Card className='expenses'>
+      <ExpenseItems
+        title={props.expenses[0].title}
+        amount={props.expenses[0].amount}
+        date={props.expenses[0].date}
+      />
+      <ExpenseItems
+        title={props.expenses[1].title}
+        amount={props.expenses[1].amount}
+        date={props.expenses[1].date}
+      />
+      <ExpenseItems
+        title={props.expenses[2].title}
+        amount={props.expenses[2].amount}
+        date={props.expenses[2].date}
+      />
+      <ExpenseItems
+        title={props.expenses[3].title}
+        amount={props.expenses[3].amount}
+        date={props.expenses[3].date}
+      />
+    </Card>
+  )
+}
